b83e9980b8ae25afa98540dc90eb73fe45fe82e8
sefi-infralight <71797160+sefi-infralight@users.noreply.github.com>
2021-07-13 13:50:24 +0300

    Codification: aws_iam_group

---
+++ infl-60ed6fefd6ca3742299cb5c6.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_group" "AGPARMDQF5IDHWKLH5C7I" {
+	name = "terraform-group"
+	path = "/"
+}
+
+
+resource "aws_iam_group" "AGPARMDQF5IDJC3762MHE" {
+	name = "CICD"
+	path = "/"
+}
